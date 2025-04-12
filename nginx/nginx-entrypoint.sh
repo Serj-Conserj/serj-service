3a2b6dac21fdbd6d0b05ed2bbe19393df65a83e6
varietytas <kuptsov.maksim2901@gmail.com>
2025-04-12 16:39:41 +0300

    Check nginx-certbot relations

--- nginx/nginx-entrypoint.sh
+++ nginx/nginx-entrypoint.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 # Prevents Nginx from starting until the certs files are available
-while [ ! -f "/etc/letsencrypt/live/conserj.ru/fullchain.pem" ] || \
-      [ ! -f "/etc/letsencrypt/live/drone.conserj.ru/fullchain.pem" ]; do
-  echo "[WAIT] Some certificates missing. Attempting again..."
-  sleep 2
-done
+# while [ ! -f "/etc/letsencrypt/live/conserj.ru/fullchain.pem" ] || \
+#       [ ! -f "/etc/letsencrypt/live/drone.conserj.ru/fullchain.pem" ]; do
+#   echo "[WAIT] Some certificates missing. Attempting again..."
+#   sleep 2
+# done
 
 # Run Nginx with global directives and daemon off
 nginx -g 'daemon off'
