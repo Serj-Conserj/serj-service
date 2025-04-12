fd5168d0c4638ffc1c8b8e146bcff3b831bbf9d2
varietytas <kuptsov.maksim2901@gmail.com>
2025-04-12 15:54:52 +0300

    Updated Docker structure for nginx and certbot

---
+++ certbot_m/certbot-entrypoint.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+EMAIL="gromaks000@gmail.com"
+DOMAINS=(
+    "conserj.ru"
+    "drone.conserj.ru"
+)
+CERT_PATH="/etc/letsencrypt/live"
+
+for i in "${!DOMAINS[@]}"; do
+  DOMAIN="${DOMAINS[$i]}"
+
+  # Check certs by ensuring respective directory exists
+  if [ -d "$CERT_PATH/$DOMAIN" ]; then
+    echo "[OK] Certificate for $DOMAIN already exists. Skipping initial issuance."
+  else
+    echo "[ISSUE] No certificates found for $DOMAIN. Attempting to issue..."
+    certbot certonly --webroot --webroot-path /var/www/certbot \
+      --email "$EMAIL" \
+      --agree-tos \
+      --no-eff-email \
+      -d "$DOMAIN"
+  fi
+done
+
+# Certs renewal loop
+echo "[LOOP] Starting certificate renewal loop..."
+while true; do
+  certbot renew --webroot --webroot-path /var/www/certbot
+  echo "[DONE] Renewal check complete. Sleeping for 24 hours..."
+  sleep 24h
+done
